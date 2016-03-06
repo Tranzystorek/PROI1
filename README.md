9ea89ffbf22a5d78644e2023060d4b94829ff4b4
Marcin Puc <marcin.e.puc@gmail.com>
2016-03-06 11:45:53 +0100

    first commit - add basic Map class and README file

---
+++ README.md
@@ -0,0 +1,3 @@
+Projekt 1.12
+
+Implementacja generatora plansz do gry "Czołgi"
---
+++ headers/Map.h
@@ -0,0 +1,14 @@
+class Map
+{
+public:
+
+    Map();
+
+    int getWidth() const {return width_;}
+    int getHeight() const {return height_;}
+
+private:
+
+    int width_;
+    int height_;
+};
