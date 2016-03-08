aa62ca9dd79e247abff89a05c586b16901a1bb3c
Marcin Puc <marcin.e.puc@gmail.com>
2016-03-09 00:38:02 +0100

    add difficulty and size properties

--- headers/Map.h
+++ headers/Map.h
@@ -1,14 +1,38 @@
+namespace Constants
+{
+    typedef enum {EASY, HARD} Difficulty;
+    //typedef enum {SMALL, MEDIUM, LARGE} Size;
+
+    struct Dim
+    {
+        Dim(int ww, int hh) : w(ww), h(hh) {};
+
+        int w;
+        int h;
+    };
+
+    const Dim SIZES[] = {Dim(50, 50), Dim(100, 100), Dim(200, 200)};
+}
+
 class Map
 {
 public:
 
-    Map();
+    Map(int w, int h, Constants::Size s, Constants::Difficulty d)
+        : width_(w), height_(h), size_(s), difficulty_(d) {};
 
     int getWidth() const {return width_;}
     int getHeight() const {return height_;}
+
+    Constants::Difficulty getDifficulty() const {return difficulty_;}
+
+    Constants::Size getSize() const {return size_};
 
 private:
 
     int width_;
     int height_;
+
+    Constants::Difficulty difficulty_;
+    Constants::Size size_;
 };
--- src/Map.cc
+++ src/Map.cc
@@ -1,6 +1,1 @@
 #include "Map.h"
-
-Map::Map()
-{
-
-}
